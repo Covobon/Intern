31eeb1dadda75ea71ac37c963c8260bfb8b2a4c4
covobon <doxuancuong96@gmail.com>
2019-05-22 18:47:38 +0700

    lam not sql nhe

---
+++ Prospring5/src/main/java/com/apress/prospring5/ch6/dao/PlainSingerDao.java
@@ -0,0 +1,136 @@
+package com.apress.prospring5.ch6.dao;
+
+import com.apress.prospring5.ch6.PlainJdbcDemo;
+import com.apress.prospring5.ch6.entities.Singer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlainSingerDao implements SingerDao {
+
+    private static Logger logger =
+            LoggerFactory.getLogger(PlainSingerDao.class);
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException ex){
+            logger.error("Problem loading DB dDriver!", ex);
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/musicbd", "prospring5", "prospring5");
+    }
+
+    private void closeConnection(Connection connection) {
+        if (connection == null){
+            return;
+        }
+
+        try{
+            connection.close();
+        }catch (SQLException ex){
+            logger.error("Problem closing connection to the database!", ex);
+        }
+    }
+
+    public List<Singer> findAll() {
+        List<Singer> result = new ArrayList<>();
+        Connection connection = null;
+
+        try {
+            connection = getConnection();
+            PreparedStatement statement =
+                    connection.prepareStatement("select * from musicbd.singer;");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next())  {
+                Singer singer = new Singer();
+                singer.setId(resultSet.getLong("id"));
+                singer.setFirstName(resultSet.getString("first_name"));
+                singer.setLastName(resultSet.getString("last_name"));
+                singer.setBirthDate(resultSet.getDate("birth_date"));
+                result.add(singer);
+                System.out.println("CUong");
+            }
+            statement.close();
+        } catch  (SQLException ex) {
+            logger.error("Problem when executing SELECT!",ex);
+        } finally {
+            closeConnection(connection);
+        }
+        return result;
+    }
+
+    @Override
+    public List<Singer> findByFirstName(String firstName) {
+        return null;
+    }
+
+    @Override
+    public String findLastNameById(Long id) {
+        return null;
+    }
+
+    @Override
+    public String findFirstNameById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void insert(Singer singer) {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "insert into musicbd.singer(first_name, last_name, birth_date) value (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, singer.getFirstName());
+            statement.setString(2, singer.getLastName());
+            statement.setDate(3, singer.getBirthDate());
+            statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                singer.setId(generatedKeys.getLong(1));
+            }
+            statement.close();
+        } catch (SQLException ex) {
+            logger.error("Prblem  executing INSERT", ex);
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    @Override
+    public void update(Singer singer) {
+
+    }
+
+    @Override
+    public void delete(Long singerId) {
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement
+                    ("delete from musicbd.singer where id=?");
+            statement.setLong(1, singerId);
+            statement.execute();
+            statement.close();
+        } catch (SQLException ex) {
+            logger.error("Prblem executing DELETE", ex);
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    @Override
+    public List<Singer> findAllWithDetial() {
+        return null;
+    }
+
+    @Override
+    public void insertWithDetail(String singer) {
+    }
+}
--- create data test.sql
+++ create data test.sql
@@ -1,27 +1,78 @@
 use intern;
+/*
+drop table fullname;
+	create table fullname(
+		id int auto_increment primary key,
+        firstname varchar(25),
+        middlename varchar(25),
+        lastname varchar(25)
+    );
+    
+    insert into fullname(firstname, middlename, lastname) values
+    ('do', 'xuan', 'cuong'),
+    ('le', 'tien', 'chieu'),
+    ('vu', 'van', 'dung');
 
 drop function if exists getId;
 
 delimiter //
-create function getId() returns int deterministic
+create function getId(maxresult int) returns int deterministic
 begin
-    declare id int unsigned default FLOOR( 1 + RAND( ) *150 );
+    declare id int unsigned default FLOOR( 1 + RAND() * maxresult );
     return id;
 end //
 delimiter ;
 
-drop function if exists generateFirstName;
+drop function if exists getFirstName;
+delimiter //
+create function getFirstName(ind int) returns varchar(25) reads sql data
+begin
+	declare result varchar(25);
+    select firstname into result from fullname where id=ind limit 1;
+    
+    return result;
+end //
 
 delimiter //
 
-create function generateFirstName(vitri int) returns varchar(255) charset latin1
+
+drop function if exists getMiddleName;
+delimiter //
+create function getMiddleName(ind int) returns varchar(25) reads sql data
 begin
-    declare min int unsigned default 1;
-    declare result varchar(255) default null;
-    set @myArrayFirstName = ('do', 'nguyen', 'tien', 'dao');
-    set result = elt(vitri, @myArrayFirstName);
+	declare result varchar(25);
+    select middlename into result from fullname where id=ind limit 1;
+    
+    return result;
+end //
+
+delimiter //
+
+drop function if exists getLastName;
+delimiter //
+create function getLastName(ind int) returns varchar(25) reads sql data
+begin
+	declare result varchar(25);
+    select lastname into result from fullname where id=ind limit 1;
+    
+    return result;
+end //
+
+delimiter ;
+*/
+
+drop table fullname;
+create table datainfo(
+	id int auto_increment primary key;
+);
+
+drop function if exists getFullName;
+delimiter //
+create function getFullName() returns varchar(25) no sql
+begin
+	declare result varchar(255);
+    set result = concat(getFirstName(getId(3))," ", getMiddleName(getId(3))," ", getLastName(getId(3)));
     return result;
 end //
 delimiter ;
-
-select generateFirstName(1);
+select getFullName();
